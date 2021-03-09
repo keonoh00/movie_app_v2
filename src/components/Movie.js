0fa67af0e66ce8a180c1b36aef33b48150ed2490
Keon Oh Kim <keonoh00@gmail.com>
2021-03-09 23:29:20 +0900

    no message

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,15 @@
 import React from "react";
+import styled from "styled-components";
 
-export default ({id}) => {
-    return id;
-}
+const Container = styled.div`
+  flex: 1 0 21%;
+  margin: 5px;
+  height: 100px;
+  background-image: url(${props => props.bgImage});
+`
+
+export default ({ id, title, image }) => {
+    return (
+        <Container bgImage={image} />
+      );
+};
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,19 +2,59 @@ import React from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/client";
 import styled from "styled-components";
+import Movie from "../components/Movie"
 
 const GET_MOVIES = gql`
     {
         movies {
             id
+            title
             medium_cover_image
         }
     }
 `;
 
-const Container = styled.div`
+const Container = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+`
+
+const Header = styled.header`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-image: linear-gradient(#DB7BFD, #FFFFFF);
+    height: 70vh;
+    width: 100%;
+`
+
+const Title = styled.h1`
+    margin-bottom: 20px;
+    font-size: 64px;
+    font-weight: 500;
     color: white;
 `
+
+const Subtitle = styled.h1`
+    font-size: 28px;
+    color: white;
+`
+
+const Loading = styled.span`
+    color: grey;
+    font-size: 30px;
+    font-weight: 400;
+`;
+
+const MovieList = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    position: relative;
+    top: -50px;
+`;
 
 export default () => {
     const { loading, error, data } = useQuery(GET_MOVIES);
@@ -28,8 +68,10 @@ export default () => {
                     Built with ReactJS, Apollo and GraphQL
                 </Subtitle>
             </Header>
-            {loading && <Loading>Loading</Loading>}
-            {!loading && data.movies && data.movies.map(m => <Movie key={m.id} id={m.id} />)}
+            {loading && <Loading>Loading...</Loading>}
+            <MovieList>
+                {!loading && data.movies && data.movies.map(m => <Movie key={m.id} id={m.id} title={m.title} image={m.medium_cover_image} />)}
+            </MovieList>
         </Container>
     )
 };
